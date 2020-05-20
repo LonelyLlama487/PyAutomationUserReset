90cf42683f5d44d828f2fa1ddf74cf176b77342c
soomjeetsahoo <61277188+soomjeetsahoo@users.noreply.github.com>
2020-05-20 18:13:37 +0530

    python project update with all required resources

--- PyFileProject.py
+++ PyFileProject.py
@@ -28,39 +28,43 @@ def sessionInitiator(uname,passkey,sec):
     except:
         print("[-]Session cannot be initiated")
 
-try:
-    userCred = credParser()
-    check = sessionInitiator(userCred['username'],userCred['password'],userCred['secret'])
-    if(check):
-        userChoice = str(input('[+]Please Enter the Name:'))
-        query = "select id,Name,Email from user where Name like '%"+userChoice+"%'";
-        op = check.query_all(query)
-        print(op['records'][0]['Name'])
-        print(op['records'][0]['Id'])
-        print(op['records'][0]['Email'])
+def passResetter(userCred,uid):
+    payload = {
+    'grant_type': 'password',
+    'client_id': userCred['consumer_key'],
+    'client_secret': userCred['consumer_secret'],
+    'username': userCred['username'],
+    'password': userCred['password'] + userCred['secret']
+    }
+    checkInstance = check.query("SELECT Id, InstanceName, IsSandbox, Name, OrganizationType FROM Organization")
+    final_url = userCred['prod_url'] if (checkInstance['records'][0]['IsSandbox'] != "False") else userCred['sandbox_url']
+    resp = req.post(final_url, data = payload, json = {})
+    
+    if (resp.ok):
+        tok = resp.json()['access_token']
+        chk_url = resp.json()['instance_url'] + "/services/apexrest/User/"
+        r = req.post(chk_url, headers = {"Authorization": "Bearer " + tok}, json = {"uid": uid})
+        print(r.json())
+    else:
+        print("[-] Connection Ended with status code :" + resp.status_code)
 
-        ch = str(input('[+]Are you sure you want to reset password for this user.(Y or N)'))
-        if(ch == 'Y' and len(op['records'][0]['Id'])>0):
-            payload = {
-                'grant_type': 'password',
-                'client_id': userCred['consumer_key'],
-                'client_secret': userCred['consumer_secret'],
-                'username': userCred['username'],
-                'password': userCred['password']+userCred['secret']
-            }
-            checkInstance = check.query("SELECT Id, InstanceName, IsSandbox, Name, OrganizationType FROM Organization")
-            final_url = userCred['prod_url'] if(checkInstance['records'][0]['IsSandbox'] != "False") else userCred['sandbox_url']
-            resp = req.post(final_url, data=payload, json={})
+if __name__ == '__main__' :
+    try:
+        userCred = credParser()
+        check = sessionInitiator(userCred['username'],userCred['password'],userCred['secret'])
+        if(check):
+            userChoice = str(input('[+]Please Enter the Name:'))
+            query = "select id,Name,Email from user where Name like '%"+userChoice+"%'";
+            op = check.query_all(query)
+            print(op['records'][0]['Name'])
+            print(op['records'][0]['Id'])
+            print(op['records'][0]['Email'])
 
-            if(resp.ok):
-                tok = resp.json()['access_token']
-                chk_url = resp.json()['instance_url']+"/services/apexrest/User/"
-                r = req.post(chk_url, headers = {"Authorization":"Bearer " + tok},json={"uid" : op['records'][0]['Id']})
-                print(r.json())
+            ch = str(input('[+]Are you sure you want to reset password for this user.(Y or N)'))
+            if(ch == 'Y' and len(op['records'][0]['Id'])>0):
+                passResetter(userCred,op['records'][0]['Id'])
             else:
-                print("[-] Connection Ended with status code :" + resp.status_code)
-        else:
-            print("[+] Session Terminated Successfully")
-except:
-    print("[-]Failed to Initiate")
+                print("[+] Session Terminated Successfully")
+    except:
+        print("[-]Failed to Initiate")
             
